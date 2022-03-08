930f943122fbc74837e144529f914fa915cc111d
Bambusgamer <96848535+Bambusgamer@users.noreply.github.com>
2022-03-08 08:55:35 +0100

    s

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const json = require('./data.json')
 
 json.forEach(data => {
     let gender
-    data.gender ? gender="r Herr" : gender=" Frau"
+    data.gender ? gender="r Herr " : gender=" Frau"
     console.log('Sehr geehrte'+ gender + data.lastname)
 });
